16ead4b8272cc52ca4602140a74d69ac5d7d46e9
Mohammed Nouri <simoiroun25@gmail.com>
2020-01-06 10:36:37 +0100

    change colors

--- script/color.js
+++ script/color.js
@@ -1,4 +1,4 @@
-class Color{
+class color{
 
 constructor(){
 
@@ -19,11 +19,18 @@ constructor(){
   this.selectColor('color4');
   });
 
+  if(localStorage.getItem("COLOR")== null){
+  document.body.style.background ='linear-gradient(#e66465, #9198e5)';
+
+  }
+
+  this.selectColor(localStorage.getItem("COLOR"));
+
 }
 
   selectColor(color){
     if(color == "color1"){
-      document.body.style.background ='linear-gradient(#e66465, #9198e5)';
+      document.body.style.background ='linear-gradient(0.25turn, #3f87a6, #ebf8e1, #7CD892)';
 
     }
         if(color == "color2"){
@@ -31,16 +38,18 @@ constructor(){
 
     }
     if(color == "color3"){
-      document.body.style.background ='linear-gradient(to left, #333, #333 50%, #eee 75%, #333 75%)';
+      document.body.style.background ='linear-gradient(0.25turn, #4FEFF4, #ebf8e1, #f69d3c)';
 
     }
     if(color == "color4"){
-      document.body.style.background ='linear-gradient(to right, black, rgba(255,0,0,1))';
+      document.body.style.background ='linear-gradient(0.25turn, #3f87a6, #ebf8e1, #EA9BF6)';
 
     }
+
+    localStorage.setItem("COLOR",color);
 
 
   }
 
 }
-onload = new Color();
+onload = new color();
